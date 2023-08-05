31e7d9ee4fc3c6d40f215156b54bfd116e9e969a
Doga Yilmaz <doga-yilmaz@outlook.com>
2023-08-05 18:56:48 +0300

    data tranformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -13,7 +15,7 @@ from dataclasses import dataclass
 class DataIngestionConfig:
     train_data_path: str= os.path.join('artifacts','train.csv')
     test_data_path: str= os.path.join('artifacts','test.csv')
-    row_data_path: str= os.path.join('artifacts','data.csv')
+    raw_data_path: str= os.path.join('artifacts','data.csv')
 
 
 class DataIngestion:
@@ -28,7 +30,7 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.row_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info('Train test split initiated')
 
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
@@ -38,14 +40,18 @@ class DataIngestion:
             logging.info('Ingestion of the data is completed')
 
             return(
+                self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
-                self.ingestion_config.test_data_path,
-                self.ingestion_config.row_data_path
+                #self.ingestion_config.raw_data_path  #???
             )
+        
         except Exception as e:
             raise CustomException(e,sys)
 
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
